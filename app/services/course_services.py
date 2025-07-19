c72e3e772e63752c151e0fcf70ff73784364b9eb
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-07-19 11:33:35 +0700

    cloudinary

--- app/services/course_services.py
+++ app/services/course_services.py
@@ -4,18 +4,18 @@ from app.models.user import Teacher
 from sqlalchemy import or_
 # from flask_sqlalchemy import Pagination
 def get_courses(page, per_page):
-    return Course.query.paginate(page=page, per_page=per_page, error_out=False)
+    return Course.query.order_by(Course.created_day.desc()).paginate(page=page, per_page=per_page, error_out=False)
 
     # return Course.query.filter_by(is_public = True).all()
 def get_courses_by_teacher_id_not_public(teacher_id, page, per_page):
-    return Course.query.filter_by(teacher_id=teacher_id, is_public=False).paginate(page=page, per_page=per_page, error_out=False)
+    return Course.query.filter_by(teacher_id=teacher_id, is_public=False).order_by(Course.created_day.desc()).paginate(page=page, per_page=per_page, error_out=False)
 
     # return Course.query.filter(
     #     Course.is_public == False,
     #     Course.teacher_id == id
     # ).all()
 def get_courses_by_teacher_id_public(teacher_id, page, per_page):
-    return Course.query.filter_by(teacher_id=teacher_id, is_public=True).paginate(page=page, per_page=per_page, error_out=False)
+    return Course.query.filter_by(teacher_id=teacher_id, is_public=True).order_by(Course.created_day.desc()).paginate(page=page, per_page=per_page, error_out=False)
 
     # return Course.query.filter(
     #     Course.is_public == True,
