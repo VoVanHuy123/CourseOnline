16d21623f9b7eb5c24e987b0d50699628d8ad87a
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-07-21 11:57:09 +0700

    test deploy 3

--- run.py
+++ run.py
@@ -9,6 +9,14 @@ app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
+        try:
+            # Test kết nối DB
+            engine = db.get_engine()
+            connection = engine.connect()
+            print("✅ Kết nối tới CSDL thành công:", engine.url)
+            connection.close()
+        except Exception as e:
+            print("❌ Không kết nối được DB:", e)
         # print(get_courses())
         db.create_all()
         app.run(debug=True)
