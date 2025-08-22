c21c6d7a37939d003f215bed1093ec11cf7a058f
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-08-22 14:31:50 +0700

    fix api update user and update course

---
+++ migrations/versions/d644375b4996_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: d644375b4996
+Revises: 7dc070d57a6e, 8f9700978716
+Create Date: 2025-08-22 10:53:23.596875
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd644375b4996'
+down_revision = ('7dc070d57a6e', '8f9700978716')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
